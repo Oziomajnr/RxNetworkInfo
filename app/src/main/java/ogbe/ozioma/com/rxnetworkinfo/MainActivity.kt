d23dfd038911f1965eb1397d33b0db57d9456a3a
oziomajnr <ogbeoziomajnr@yahoo.com>
2019-12-06 22:52:15 +0100

    update demo code to version 2

--- app/src/main/java/ogbe/ozioma/com/rxnetworkinfo/MainActivity.kt
+++ app/src/main/java/ogbe/ozioma/com/rxnetworkinfo/MainActivity.kt
@@ -1,20 +1,18 @@
 package ogbe.ozioma.com.rxnetworkinfo
 
-import android.content.Context
-import android.net.ConnectivityManager
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import ogbe.ozioma.com.rxnetworkinfo2.InternetConnectionHelper
 
 class MainActivity : AppCompatActivity() {
 
 
     private val internetConnectionHelper: InternetConnectionHelper by lazy {
-        InternetConnectionHelper.Companion.getInstance(this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+        InternetConnectionHelper.Companion.getInstance(this)
     }
 
     val disposable: CompositeDisposable = CompositeDisposable()
