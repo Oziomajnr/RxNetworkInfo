75d0335244b449ac06b0fa5a2216fc10d443ebd0
oziomajnr <ogbeoziomajnr@yahoo.com>
2019-12-06 22:33:48 +0100

    add code to demo project

--- app/src/main/java/ogbe/ozioma/com/rxnetworkinfo/MainActivity.kt
+++ app/src/main/java/ogbe/ozioma/com/rxnetworkinfo/MainActivity.kt
@@ -1,12 +1,37 @@
 package ogbe.ozioma.com.rxnetworkinfo
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import ogbe.ozioma.com.rxnetworkinfo2.InternetConnectionHelper
 
 class MainActivity : AppCompatActivity() {
 
+
+    private val internetConnectionHelper: InternetConnectionHelper by lazy {
+        InternetConnectionHelper.Companion.getInstance(this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
+    }
+
+    val disposable: CompositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        disposable.add(internetConnectionHelper.internetConnectionAvailable.observeOn(
+            AndroidSchedulers.mainThread()
+        )
+            .subscribe {
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            })
+    }
+
+    override fun onDestroy() {
+        disposable.dispose()
+        super.onDestroy()
     }
 }
